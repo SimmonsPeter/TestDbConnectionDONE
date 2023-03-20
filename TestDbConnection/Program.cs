1ebd4941e8729042debb2f368b1fd207b9fa7a41
SimmonsPeter <pierrelucsimoneau@outlook.com>
2023-03-20 15:53:05 -0400

    Ajoutez des fichiers projet.

---
+++ TestDbConnection/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//construction de la chaine de connection
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+string strConnection = "Data Source=(local);"//nom du serveur
+    + "Initial Catalog=Northwind;"//nom de la base de donné
+    +"Integrated Security=SSPI";//authentification windows
+
+//creation de l'objet connection
+SqlConnection myConnection = new SqlConnection(strConnection);
+//se connecter a la base de onnee
+try
+{
+    myConnection.Open();
+    Console.WriteLine("Connexion établie");
+}catch(Exception ex)
+{
+    Console.WriteLine("Connexion échouée");
+    Console.WriteLine(ex.Message);
+}
